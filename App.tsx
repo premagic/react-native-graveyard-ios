b66c1730b59718167faf3b91db423cde37c3fae3
Anenth <anenthvishnu@gmail.com>
2023-03-22 14:53:15 +0530

    Check permission on ios

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { Component, React } from 'react';
 import {
   Dimensions, PermissionsAndroid, SafeAreaView, StatusBar, StyleSheet,
   Text, useColorScheme,
-  View
+  View, Platform
 } from 'react-native';
 import { WebView } from 'react-native-webview';
 
@@ -23,27 +23,30 @@ type SectionProps = PropsWithChildren<{
 }>;
 
 const requestCameraPermission = async () => {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.CAMERA,
-      {
-        title: 'Cool Photo App Camera Permission',
-        message:
+  if (Platform.OS === 'android') {
+
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
           'Cool Photo App needs access to your camera ' +
           'so you can take awesome pictures.',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK',
-      },
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('You can use the camera');
-    } else {
-      console.log('Camera permission denied');
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use the camera');
+        } else {
+          console.log('Camera permission denied');
+        }
+      } catch (err) {
+        console.warn(err);
+      }
     }
-  } catch (err) {
-    console.warn(err);
-  }
 };
 
 function App(): JSX.Element {
